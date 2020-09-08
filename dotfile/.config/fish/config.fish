999f339b882c0b6f52773c31b02d64c4adaaa8b8
yutanakano <yutanakano395@icloud.com>
2020-09-09 01:46:25 +0900

    AndroidStudioにpathを通す

--- dotfile/.config/fish/config.fish
+++ dotfile/.config/fish/config.fish
@@ -7,6 +7,18 @@ eval (direnv hook fish)
 #fisherパッケージdecors/fish-ghqの設定
 set GHQ_SELECTOR peco
 
+#-- AndroidStudioのpathを通す
+set --export ANDROID $HOME/Library/Android;
+set --export ANDROID_HOME $ANDROID/sdk;
+set -gx PATH $ANDROID_HOME/tools $PATH;
+set -gx PATH $ANDROID_HOME/tools/bin $PATH;
+set -gx PATH $ANDROID_HOME/platform-tools $PATH;
+set -gx PATH $ANDROID_HOME/emulator $PATH
+
+set --export JAVA_HOME /Applications/Android\ Studio.app/Contents/jre/jdk/Contents/Home;
+set -gx PATH $JAVA_HOME/bin $PATH;
+#--
+
 # キーバインドの設定
 function fish_user_key_bindings
   # pecoでhistoryをインクリメンタルに検索できるやつ。便利すぎる。
