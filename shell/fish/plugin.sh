abfb9f754aee5f91647b6f702e0fccffa988159b
yutanakano <yutanakano395@gmail.com>
2021-04-04 08:27:29 +0900

    コマンド体系が変わったので変更 (#4)

--- shell/fish/plugin.sh
+++ shell/fish/plugin.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # POSIX非互換を解決する。便利。
-fisher add edc/bass
+fisher install edc/bass
 # 過去に訪れたディレクトリに一発で飛べる。便利。
-fisher add jethrokuan/z
+fisher install jethrokuan/z
 # ディレクトリを簡単に遡れるやつ。これも便利。
-fisher add 0rax/fish-bd
+fisher install 0rax/fish-bd
 # pecoを利用するために必要なプラグインを追加
-fisher add oh-my-fish/plugin-peco
+fisher install oh-my-fish/plugin-peco
 # z コマンドで頻繁に訪れるディレクトリを peco で選択して移動する。
-fisher add tsu-nera/fish-peco_recentd
+fisher install tsu-nera/fish-peco_recentd
 # ghqの補完とキーバインド(Control g)
-fisher add decors/fish-ghq
+fisher install decors/fish-ghq
